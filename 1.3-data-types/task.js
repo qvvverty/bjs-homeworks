ad2586828264d06d5d806751d94e0363eec64191
Leonid Kostyuchenko <kstchnko@gmail.com>
2020-06-16 03:58:00 +0300

    task 1.3-data-types completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,32 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+  if (isNaN(percent)) {
+    return 'Параметр "процентная ставка" пуст или содержит нечисловое значение';
+  } else if (isNaN(contribution)) {
+    return 'Параметр "cумма первоначального взноса" пуст или содержит нечисловое значение';
+  } else if (isNaN(amount)) {
+    return 'Параметр "сумма кредита" пуст или содержит нечисловое значение';
+  }
+
+  let loanBody = amount - contribution;
+  let currentDate = new Date;
+  let months = (date.getFullYear() - currentDate.getFullYear()) * 12 + date.getMonth() - currentDate.getMonth();
+  let interestRate = percent / 100 / 12;
+  let monthlyPayment = loanBody * (interestRate + interestRate / (Math.pow(1 + interestRate, months) - 1));
+  let totalAmount = Number((monthlyPayment * months).toFixed(2));
+
+  console.log(totalAmount);
+  return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  if (!name) {
+    name = 'Аноним';
+  }
+
+  let greeting = `Привет, мир! Меня зовут ${name}`;
+
+  console.log(greeting);
+  return greeting;
 }
